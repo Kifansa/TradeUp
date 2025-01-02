3ab001a19e0233a08c4561027b64146a6b8c8e44
Kifansa <kifansafadhlurrohman5@gmail.com>
2025-01-02 15:42:04 +0700

    finishing

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'dcb9eee0ee504ba0cbb969410ae10db28079fdc5',
+        'reference' => 'dc6f208405af6079ac592c2505e6fa29cbd0fb39',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -382,7 +382,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'dcb9eee0ee504ba0cbb969410ae10db28079fdc5',
+            'reference' => 'dc6f208405af6079ac592c2505e6fa29cbd0fb39',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
